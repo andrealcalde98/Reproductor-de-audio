008cfe0e16a7052f3a20ba9145668c96f88aad4c
papidre <andrealcalde2998@gmail.com>
2019-05-02 12:46:56 +0200

    diseño inicial a falta de slider

--- src/m06/uf1/audioplayer/Vista.java
+++ src/m06/uf1/audioplayer/Vista.java
@@ -1,15 +1,26 @@
 package m06.uf1.audioplayer;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class Vista {
 
     private JFrame finestra;
-    private JPanel panell;
-
+    private JPanel panellsup;
+    private JPanel panellinf;
+    private JLabel reproduint;
+    private JComboBox listas;
+    private JTable canciones;
     private JButton play;
     private JButton stop;
     private JButton pausa;
@@ -18,22 +29,39 @@ public class Vista {
     public Vista() {
 
         finestra = new JFrame("Reproductor Àudio");
-        finestra.setSize(400, 50);
-        finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        finestra.setResizable(false);
+        finestra.setSize(1200, 800);
+        //finestra.setResizable(false);
         finestra.setLocationRelativeTo(null);
-        panell = new JPanel();
-        panell.setLayout(new GridLayout(1, 5));
+        finestra.setLayout(new BoxLayout(finestra.getContentPane(),BoxLayout.Y_AXIS));
+        //superior
+        panellsup = new JPanel();
+        panellsup.setLayout(new GridLayout(1, 3));
+        reproduint = new JLabel("cancion en reproduccion");
+        listas = new JComboBox();
+        canciones = new JTable();
+        panellsup.add(canciones);
+        panellsup.add(reproduint);
+        panellsup.add(listas);
+        //inferior
+        panellinf = new JPanel();
+        panellinf.setLayout(new GridLayout(1,4));
         play = new JButton("Play");
         stop = new JButton("Stop");
         pausa = new JButton("Pause");
-        continuar = new JButton("Continue");     
-        panell.add(play);
-        panell.add(pausa);
-        panell.add(continuar);
-        panell.add(stop);
-        finestra.add(panell);
-        finestra.setVisible(true);
+        continuar = new JButton("Continue");
+        panellinf.add(play);
+        panellinf.add(pausa);
+        panellinf.add(continuar);
+        panellinf.add(stop);
+       
+        
+                    
+        finestra.add(panellsup);
+        finestra.add(panellinf);
+        finestra.pack();
+        finestra.setVisible(true);        
+        finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 
 
@@ -46,11 +74,11 @@ public class Vista {
     }
 
     public JPanel getPanell() {
-        return panell;
+        return panellsup;
     }
 
     public void setPanell(JPanel panell) {
-        this.panell = panell;
+        this.panellsup = panell;
     }
 
     public JButton getPlay() {
