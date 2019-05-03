fd48f022e267e25bfe77c323ed2ff105a795b5e4
papidre <andrealcalde2998@gmail.com>
2019-05-03 13:03:36 +0200

    Jtable modelo

--- src/m06/uf1/audioplayer/ReproductorAudio.java
+++ src/m06/uf1/audioplayer/ReproductorAudio.java
@@ -13,6 +13,6 @@ import java.io.FileNotFoundException;
 
 public class ReproductorAudio {
     public static void main(String[] args) throws FileNotFoundException {
-        Controlador controlador = new Controlador();     
+        Vista vista = new Vista();     
     }
 }
