4e4ca118813fdbefcbf97d3b07b12d196fbdfb3c
adri <adri.d.m1997@gmail.com>
2019-05-04 23:12:44 +0200

    leer json hecho

--- src/m06/uf1/audioplayer/LlegeixJSON.java
+++ src/m06/uf1/audioplayer/LlegeixJSON.java
@@ -5,9 +5,11 @@
  */
 package m06.uf1.audioplayer;
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -18,17 +20,40 @@ import org.json.simple.parser.ParseException;
  * @author jmartin
  */
 public class LlegeixJSON {
+private static final String filePath = "json/lista1.json";
+    public static void main(String[] args) {
+        try{
+            JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(filePath));
+        JSONArray json = (JSONArray) obj;
+        for (int i = 0; i<json.size();i++){
+            JSONObject object = (JSONObject) json.get(i);
+            String nom = object.get("nom").toString();
+            String autor = object.get("autor").toString();
+            String album = object.get("album").toString();
+            String durada = object.get("durada").toString();
+            String ruta = object.get("ruta").toString();
+            System.out.println(nom);
+            System.out.println(autor);
+            System.out.println(album);
+            System.out.println(durada);
+            System.out.println(ruta);
+            }
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
-    public static void main(String[] args) throws ParseException, FileNotFoundException, IOException {
-        JSONParser parser = new JSONParser();
-        
-            JSONObject cançons = (JSONObject) parser.parse(new FileReader("json/cançons.json"));
-            System.out.println(cançons.get("nom"));
-            System.out.println(cançons.get("autor"));
-            System.out.println(cançons.get("àlbum"));
-            System.out.println(cançons.get("durada"));
-            System.out.println(cançons.get("ruta"));
-    }        
 }
     
 
