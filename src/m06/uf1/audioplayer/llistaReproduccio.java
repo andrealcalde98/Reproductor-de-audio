50d3b0f455e5cfdfb763083ff4e42b7d731f9fe2
papidre <andrealcalde2998@gmail.com>
2019-06-12 19:39:47 +0200

    errors git

--- src/m06/uf1/audioplayer/llistaReproduccio.java
+++ src/m06/uf1/audioplayer/llistaReproduccio.java
@@ -1,23 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package m06.uf1.audioplayer;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class llistaReproduccio {
-
     private String nom;
+    private String num;
     private ArrayList canciones;
 
-    public llistaReproduccio(String nom, ArrayList<Cancion> canciones) {
+    public llistaReproduccio(String nom, String num, ArrayList<Cancion> canciones) {
         this.nom = nom;
         this.canciones = canciones;
+        this.num = num;
     }
-    public llistaReproduccio(){}
+
+    public llistaReproduccio() {
+    }
 
     public String getNom() {
         return nom;
@@ -27,8 +24,6 @@ public class llistaReproduccio {
         this.nom = nom;
     }
 
- 
-
     public ArrayList getCanciones() {
         return canciones;
     }
@@ -37,9 +32,16 @@ public class llistaReproduccio {
         this.canciones = canciones;
     }
 
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
     @Override
     public String toString() {
         return "llistaReproduccio{" + "nom=" + nom + ", canciones=" + canciones + '}';
     }
-
 }
