0e20309f8f0cddfc9212b22c0f938ec5e845ff41
adri <adri.d.m1997@gmail.com>
2019-06-12 15:09:27 +0200

    llegeix json

--- src/m06/uf1/audioplayer/LlegeixJSON.java
+++ src/m06/uf1/audioplayer/LlegeixJSON.java
@@ -8,44 +8,33 @@ package m06.uf1.audioplayer;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class LlegeixJSON {
-private static final String filePath = "json/lista1.json";
-    public static void main(String[] args) {
-        try{
-            JSONParser parser = new JSONParser();
-        Object obj = parser.parse(new FileReader(filePath));
-        JSONArray json = (JSONArray) obj;
-        for (int i = 0; i<json.size();i++){
-            JSONObject object = (JSONObject) json.get(i);
-            String nom = object.get("nom").toString();
-            String autor = object.get("autor").toString();
-            String album = object.get("album").toString();
-            String durada = object.get("durada").toString();
-            String ruta = object.get("ruta").toString();
-            System.out.println(nom);
-            System.out.println(autor);
-            System.out.println(album);
-            System.out.println(durada);
-            System.out.println(ruta);
-            }
-        }
-        catch(FileNotFoundException e){
-            e.printStackTrace();
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-        catch(ParseException e){
-            e.printStackTrace();
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-    }
+    public static llistaReproduccio LlegeixJSON(int opt) {
+        JSONParser parser = new JSONParser();
+        llistaReproduccio llistareproduccio = null;
+        String nom;
+        ArrayList can;
 
+        try {
+//            ArrayList<Cancion> canciones = new ArrayList();
+            JSONObject llista1 = (JSONObject) parser.parse(new FileReader("llista" + opt + ".json"));
+            //System.out.println(llista1.get("nom"));
+
+            nom = (String) llista1.get("nom");
+
+            can = (ArrayList) llista1.get("llistatJSON");
+
+            llistareproduccio = new llistaReproduccio(nom, can);
+
+        } catch (FileNotFoundException e) {
+        } catch (IOException | ParseException e) {
+        }
+        return llistareproduccio;
+    }
 }
